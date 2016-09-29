6459c9b0c642f38d45f98567f908e6fb050a9b44
Eric Satterwhite <esatterwhite@wi.rr.com>
2016-09-28 21:56:08 -0500

    elector test
    1 node should elect itself

--- test/test.spec.js
+++ test/test.spec.js
@@ -2,13 +2,25 @@
 const assert = require('assert');
 const sinon  = require('sinon')
 const zmqbus = require('../');
+const Elector = require('../lib/election');
 
 describe('zmqbus',function(){
     describe('createNode', function(){
-        it('should create a new elected node', function(done){
-            let n1 = zmqbus.createNode({election_timeout: 250});
-            let cb = sinon.spy();
+		let n1;
+		before(function( done ){
+			n1 = zmqbus.createNode({election_timeout: 250});
             n1.subscribe('chan1');
+			done();
+		});
+
+		after(function( done ){
+			n1.elector.stop();
+			n1 = null;
+			done();
+		});
+
+        it('should create a new elected node', function(done){
+            let cb = sinon.spy();
 
             n1.on('ready',()=>{
                 
@@ -24,6 +36,34 @@ describe('zmqbus',function(){
             });
             n1.on('message', cb);
         });
-        
-    })
+
+	})
+
+	describe('Elector',function(){
+		let elector;
+		before(function(done){
+			elector = new Elector({election_timeout: 2000, multicast_port:55555})
+		});
+		
+		after(function(done){
+			elector.stop();
+			elector = null;
+		});
+
+		it('should elect it self', function( done ){
+			this.timeout( 6000);
+			
+			elector.on('ready', (addr) =>{
+				elector.start()
+			})
+
+			elector.on('error', done)
+
+			elector.on('elected', (elect) =>{
+				assert.equal(elect.id, elector.id);
+				console.log('elected');
+				done()
+			})
+		});
+	})
 })
